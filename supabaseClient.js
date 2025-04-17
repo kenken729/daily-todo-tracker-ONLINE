22c25e1e433ffb4566731554d38ebc3da47647dc
kenken729 <kokoduck@gmail.com>
2025-04-18 02:30:59 +0800

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://uaynxcgnbwivxmkkkpqe.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVheW54Y2duYndpdnhta2trcHFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5MDkyMTMsImV4cCI6MjA2MDQ4NTIxM30.7SS7iuTet6bdsCnYtmRlqEMc4DzzAzJfShNVVMrBnyo';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
