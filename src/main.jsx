2eb0606a2a0b49b00dd07b763d36b86f150e9a45
kenken729 <kokoduck@gmail.com>
2025-04-18 02:29:29 +0800

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DailyWorkReminderApp from './DailyWorkReminderApp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DailyWorkReminderApp />
+  </React.StrictMode>
+)
